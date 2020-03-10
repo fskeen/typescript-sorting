3795f4e7e1fcb0645dab8742d26ba35b852de7e9
Faye Skeen <fskeen@gmail.com>
2020-03-10 16:56:45 -0700

    finished num collection, added Sortable interface to Sorter class

---
+++ src/NumCollection.ts
@@ -0,0 +1,19 @@
+export class NumCollection {
+  data: number[];
+  constructor(data: number[]) {
+    this.data = data;
+  }
+  get length(): number {
+    // this is the GET keyword! It lets us use this function to get the correct length of the data, but ALSO lets us say "thing.length" and refer to the returned value of this function. It's like thing.getLength(), but you refer to it like you would a property (thing.length)
+    return this.data.length;
+  }
+  compare(leftIndex: number, rightIndex: number): boolean {
+    // returns true/false if 2 elements next to each other need to be swapped
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+  swap(leftIndex: number, rightIndex: number): void {
+    const currentNum = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = currentNum;
+  }
+}
---
+++ src/Sorter.ts
@@ -0,0 +1,21 @@
+import { NumCollection } from "./NumCollection";
+
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
+
+export class Sorter {
+  constructor(public collection: Sortable) {}
+  sort(): void {
+    const length = this.collection.length;
+    for (let i = 0; i < length; i++) {
+      for (let x = 0; x < length - i - 1; x++) {
+        if (this.collection.compare(x, x + 1)) {
+          this.collection.swap(x, x + 1);
+        }
+      }
+    }
+  }
+}
